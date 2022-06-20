50cc30b7a5dd14f059ca9de0a78cfe045160cfbd
Eric <yuukisouma2003@gmail.com>
2022-06-20 15:38:58 -0300

    feat: Pasta 002 criada, variáveis em java

---
+++ aula002/Variaveis.java
@@ -0,0 +1,10 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Eric";
+        int idade = 19;
+        int quantosAnos = 10;
+        int idadeNoFuturo = idade + quantosAnos;
+        System.out.println("Olá " + nome + ", seja bem vindo");
+        System.out.print("Daqui a " + quantosAnos + " Você vai ter: " + idadeNoFuturo);
+    }
+}
