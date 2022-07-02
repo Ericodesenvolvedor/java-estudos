00d01958d21dc57fd2b72e665230d5088d18dd4b
Eric <yuukisouma2003@gmail.com>
2022-07-02 18:58:32 -0300

    feat: aula005: estruturas de repetição

---
+++ aula005/src/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Hello world");
+            count++;
+        } while (count <= 10);
+    }
+}
